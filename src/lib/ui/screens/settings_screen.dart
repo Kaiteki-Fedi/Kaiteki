9e0d3317ea03d42f88650d2a55e93a94c39635c4
Craftplacer <craftplacer@outlook.de>
2020-09-08 23:32:50 +0200

    Refactored settings screen to dynamically build widgets based on data

--- src/lib/ui/screens/settings_screen.dart
+++ src/lib/ui/screens/settings_screen.dart
@@ -3,74 +3,126 @@ import 'package:kaiteki/ui/widgets/separator_text.dart';
 import 'package:mdi/mdi.dart';
 
 class SettingsScreen extends StatelessWidget {
+  static var _sections = <_Section>[
+    _Section(
+      items: [
+        const _SettingsItem(
+          icon: Mdi.wrench,
+          title: "General"
+        ),
+        const _SettingsItem(
+          icon: Mdi.account,
+          title: "Profile"
+        ),
+        const _SettingsItem(
+          icon: Mdi.lock,
+          title: "Security"
+        ),
+        const _SettingsItem(
+          icon: Mdi.filter,
+          title: "Filtering"
+        ),
+        const _SettingsItem(
+          icon: Mdi.bell,
+          title: "Notifications"
+        ),
+        const _SettingsItem(
+          icon: Mdi.download,
+          title: "Data Import / Export"
+        ),
+        const _SettingsItem(
+          icon: Mdi.eyeOff,
+          title: "Mutes and Blocks",
+        ),
+      ],
+    ),
+    _Section(
+      title: "Kaiteki Settings",
+      items: [
+        _SettingsItem(
+          icon: Mdi.palette,
+          title: "Customization",
+          onTap: (context) => Navigator.pushNamed(context, "/settings/customization"),
+        ),
+        const _SettingsItem(
+          icon: Mdi.tab,
+          title: "Tabs",
+        ),
+      ],
+    ),
+    _Section(
+      items: [
+        _SettingsItem(
+          icon: Mdi.information,
+          title: "About",
+          onTap: (c) => Navigator.pushNamed(c, "/about")
+        ),
+        _SettingsItem(
+          icon: Mdi.bug,
+          title: "Debug and maintenance",
+          onTap: (c) => Navigator.pushNamed(c, "/debug")
+        )
+      ]
+    )
+  ];
+
+  // GridView for desktop impl.
+  List<Widget> _getListItems(BuildContext context, List<_Section> sections) {
+    var widgets = <Widget>[];
+
+    for (int i = 0; i < sections.length; i++) {
+      if (i > 0)
+        widgets.add(Divider());
+
+      var section = sections[i];
+
+      if (section.hasTitleHeader)
+        widgets.add(SeparatorText(section.title));
+
+      for (var item in section.items)
+        widgets.add(
+            ListTile(
+              leading: Icon(item.icon),
+              title: Text(item.title),
+              enabled: item.isEnabled,
+              onTap: () => item.onTap.call(context),
+            )
+        );
+    }
+
+    return widgets;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Settings"),
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            leading: Icon(Mdi.wrench),
-            title: Text("General"),
-            enabled: false,
-          ),
-          ListTile(
-            leading: Icon(Mdi.account),
-            title: Text("Profile"),
-            enabled: false,
-          ),
-          ListTile(
-            leading: Icon(Mdi.lock),
-            title: Text("Security"),
-            enabled: false,
-          ),
-          ListTile(
-            leading: Icon(Mdi.filter),
-            title: Text("Filtering"),
-            enabled: false,
-          ),
-          ListTile(
-            leading: Icon(Mdi.bell),
-            title: Text("Notifications"),
-            enabled: false,
-          ),
-          ListTile(
-            leading: Icon(Mdi.download),
-            title: Text("Data Import / Export"),
-            enabled: false,
-          ),
-          ListTile(
-            leading: Icon(Mdi.eyeOff),
-            title: Text("Mutes and Blocks"),
-            enabled: false,
-          ),
-          Divider(),
-          SeparatorText("Kaiteki Settings"),
-          ListTile(
-            leading: Icon(Mdi.palette),
-            title: Text("Customization"),
-            onTap: () => Navigator.pushNamed(context, "/settings/customization"),
-          ),
-          ListTile(
-            leading: Icon(Mdi.tab),
-            title: Text("Tabs"),
-            enabled: false,
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(Mdi.information),
-            title: Text("About"),
-            onTap: () => Navigator.pushNamed(context, "/about")
-          ),
-          ListTile(
-            leading: Icon(Mdi.bug),
-            title: Text("Debug and maintenance"),
-            onTap: () => Navigator.pushNamed(context, "/debug")
-          )
-        ],
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return ListView(children: _getListItems(context, _sections));
+        },
       )
     );
   }
+}
+
+class _Section {
+  final String title;
+  final List<_SettingsItem> items;
+
+  bool get hasTitleHeader => title != null;
+
+  const _Section({this.title, @required this.items});
+}
+
+class _SettingsItem {
+  final IconData icon;
+  final String title;
+  final Function(BuildContext context) onTap;
+
+  bool get isEnabled => onTap != null;
+
+  const _SettingsItem({@required this.icon, @required this.title, this.onTap});
 }
