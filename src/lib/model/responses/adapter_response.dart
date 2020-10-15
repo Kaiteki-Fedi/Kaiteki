0dbb4a3bd33eaddd71c4d1011c7a28539a7edf8a
Craftplacer <craftplacer@outlook.de>
2020-10-15 13:55:28 +0200

    Remove unused classes

--- src/lib/model/responses/adapter_response.dart
+++
@@ -1,7 +0,0 @@
-import 'package:kaiteki/adapters/fediverse_adapter.dart';
-
-abstract class AdapterResponse<Adapter extends FediverseAdapter> {
-  final Adapter source;
-
-  const AdapterResponse(this.source);
-}
