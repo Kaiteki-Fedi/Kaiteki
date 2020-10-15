fd55ca99461bf699acbad3bc9f375a845a2e0409
Craftplacer <craftplacer@outlook.de>
2020-10-15 13:59:10 +0200

    Add case-insentitive equals extension method

--- src/lib/utils/string_extensions.dart
+++ src/lib/utils/string_extensions.dart
@@ -1,4 +1,8 @@
 extension StringExtensions on String {
   bool get isNullOrEmpty => this == null || this.isEmpty;
   bool get isNotNullOrEmpty => !isNullOrEmpty;
+
+  bool equalsIgnoreCase(String other) {
+    return this?.toLowerCase() == other?.toLowerCase();
+  }
 }
