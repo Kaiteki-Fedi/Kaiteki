1aa7f53331946186775e5da8561b296f34e61ee0
Craftplacer <craftplacer@outlook.de>
2020-10-26 04:40:42 +0100

    make pleroma and mastodon share same base code

--- src/kaiteki/lib/api/adapters/pleroma_adapter.c.dart
+++ src/kaiteki/lib/api/adapters/pleroma_adapter.c.dart
@@ -1,45 +1,7 @@
 part of 'pleroma_adapter.dart';
 
-Post toPost(MastodonStatus source) {
-  if (source == null) return null;
-
-  return Post(
-    source: source,
-    content: source.content,
-    postedAt: source.createdAt,
-    nsfw: source.sensitive,
-    subject: source.spoilerText,
-    author: toUser(source.account),
-    repeatOf: toPost(source.reblog),
-    repeated: source.reblogged,
-    liked: source.favourited,
-    emojis: source.emojis.map(toEmoji),
-  );
-}
-
-CustomEmoji toEmoji(MastodonEmoji emoji) {
-  return CustomEmoji(
-    source: emoji,
-    url: emoji.staticUrl,
-    name: emoji.shortcode,
-    aliases: emoji.tags,
-  );
-}
-
-User toUser(MastodonAccount source) {
-  if (source == null) return null;
-
-  return User(
-    source: source,
-    displayName: source.displayName,
-    username: source.username,
-    bannerUrl: source.header,
-    avatarUrl: source.avatar,
-    joinDate: source.createdAt,
-    id: source.id,
-
-  );
-}
-
 // TODO
-ChatMessage toChatMessage(PleromaChatMessage message) => throw UnimplementedError();
+ChatMessage toChatMessage(PleromaChatMessage message) {
+  // TODO: implement toChatMessage
+  throw UnimplementedError();
+}
--- src/kaiteki/lib/api/adapters/pleroma_adapter.dart
+++ src/kaiteki/lib/api/adapters/pleroma_adapter.dart
@@ -1,142 +1,22 @@
-import 'package:kaiteki/account_container.dart';
-import 'package:kaiteki/api/adapters/fediverse_adapter.dart';
+import 'package:fediverse_objects/pleroma/chat_message.dart';
 import 'package:kaiteki/api/adapters/interfaces/chat_support.dart';
 import 'package:kaiteki/api/adapters/interfaces/reaction_support.dart';
+import 'package:kaiteki/api/adapters/shared_mastodon_adapter.dart';
 import 'package:kaiteki/api/clients/pleroma_client.dart';
-import 'package:fediverse_objects/mastodon/account.dart';
-import 'package:fediverse_objects/mastodon/emoji.dart';
-import 'package:fediverse_objects/mastodon/status.dart';
-import 'package:fediverse_objects/pleroma/chat_message.dart';
-import 'package:kaiteki/model/auth/account_secret.dart';
-import 'package:kaiteki/model/auth/authentication_data.dart';
-import 'package:kaiteki/model/auth/login_result.dart';
 import 'package:kaiteki/model/fediverse/chat.dart';
 import 'package:kaiteki/model/fediverse/chat_message.dart';
 import 'package:kaiteki/model/fediverse/emoji.dart';
-import 'package:kaiteki/model/fediverse/notification.dart';
 import 'package:kaiteki/model/fediverse/post.dart';
 import 'package:kaiteki/model/fediverse/reaction.dart';
-import 'package:kaiteki/model/fediverse/timeline_type.dart';
 import 'package:kaiteki/model/fediverse/user.dart';
-import 'package:kaiteki/auth/login_functions.dart';
-import 'package:kaiteki/model/auth/account_compound.dart';
-import 'package:kaiteki/utils/extensions/string.dart';
 
-part 'pleroma_adapter.c.dart'; // That file contains toEntity() methods
+part 'pleroma_adapter.c.dart';
 
-class PleromaAdapter extends FediverseAdapter<PleromaClient> implements ChatSupport, ReactionSupport {
+class PleromaAdapter extends SharedMastodonAdapter<PleromaClient> implements ChatSupport, ReactionSupport {
   PleromaAdapter._(PleromaClient client) : super(client);
 
   factory PleromaAdapter({PleromaClient client}) {
     return PleromaAdapter._(client ?? PleromaClient());
-  }
-
-  @override
-  Future<User> getUserById(String id) async {
-    return toUser(await client.getAccount(id));
-  }
-
-  @override
-  Future<LoginResult> login(String instance, String username, String password, mfaCallback, AccountContainer accounts) async {
-    client.instance = instance;
-
-    // Retrieve or create client secret
-    var clientSecret = await LoginFunctions.getClientSecret(client, instance, accounts.getClientRepo());
-    client.authenticationData = MastodonAuthenticationData();
-    client.authenticationData.clientSecret = clientSecret.clientSecret;
-    client.authenticationData.clientId = clientSecret.clientId;
-
-    String accessToken;
-
-    // Try to login and handle error
-    var loginResponse = await client.login(username, password);
-    accessToken = loginResponse.accessToken;
-
-    if (loginResponse.error.isNotNullOrEmpty) {
-      if (loginResponse.error != "mfa_required") {
-        return LoginResult.failed(loginResponse.error);
-      }
-
-      var code = await mfaCallback.call();
-
-      if (code == null)
-        return LoginResult.aborted();
-
-      // TODO: add error-able TOTP screens
-      // TODO: make use of a while loop to make this more efficient
-      var mfaResponse = await client.respondMfa(
-        loginResponse.mfaToken,
-        int.parse(code),
-      );
-
-      if (mfaResponse.error.isNotNullOrEmpty) {
-        return LoginResult.failed(mfaResponse.error);
-      } else {
-        accessToken = mfaResponse.accessToken;
-      }
-    }
-
-    // Create and set account secret
-    var accountSecret = new AccountSecret(instance, username, accessToken);
-    client.authenticationData.accessToken = accountSecret.accessToken;
-
-    // Check whether secrets work, and if we can get an account back
-    var account = await client.verifyCredentials();
-    if (account == null) {
-      return LoginResult.failed("Failed to verify credentials");
-    }
-
-    var compound = AccountCompound(
-      container: accounts,
-      adapter: this,
-      account: toUser(account),
-      clientSecret: clientSecret,
-      accountSecret: accountSecret,
-    );
-    await accounts.addCurrentAccount(compound);
-
-    return LoginResult.successful();
-  }
-
-  @override
-  Future<Post> postStatus(Post post, {Post parentPost}) {
-    // TODO: implement postStatus
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<User> getMyself() async {
-    var account = await client.verifyCredentials();
-    return toUser(account);
-  }
-
-  @override
-  Future<Iterable<ChatMessage>> getChatMessages(Chat chat) {
-    // TODO: implement getChatMessages
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Iterable<Chat>> getChats() {
-    // TODO: implement getChats
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Iterable<Notification>> getNotifications() {
-    // TODO: implement getNotifications
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Iterable<Post>> getStatusesOfUserById(String id) async {
-    return (await client.getStatuses(id)).map((mst) => toPost(mst));
-  }
-
-  @override
-  Future<Iterable<Post>> getTimeline(TimelineType type) async {
-    var posts = await client.getTimeline();
-    return posts.map((m) => toPost(m));
   }
 
   @override
@@ -160,7 +40,8 @@ class PleromaAdapter extends FediverseAdapter<PleromaClient> implements ChatSupp
 
   @override
   Future<void> react(Post post, Emoji emoji) {
-
+    // TODO: implement react
+    throw UnimplementedError();
   }
 
   @override
@@ -168,4 +49,16 @@ class PleromaAdapter extends FediverseAdapter<PleromaClient> implements ChatSupp
 
   @override
   bool supportsUnicodeEmoji = true;
+
+  @override
+  Future<Iterable<ChatMessage>> getChatMessages(Chat chat) {
+    // TODO: implement getChatMessages
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Iterable<Chat>> getChats() {
+    // TODO: implement getChats
+    throw UnimplementedError();
+  }
 }
