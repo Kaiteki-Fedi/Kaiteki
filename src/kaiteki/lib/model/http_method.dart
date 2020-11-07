711a19892ef938d2d3e88eba83aabaeea2f29e91
Craftplacer <craftplacer@outlook.de>
2020-11-07 20:15:14 +0100

    Add PUT and DELETE HTTP methods to enum

--- src/kaiteki/lib/model/http_method.dart
+++ src/kaiteki/lib/model/http_method.dart
@@ -1,14 +1,23 @@
 enum HttpMethod {
   GET,
-  POST
+  POST,
+  PUT,
+  DELETE,
 }
 
 extension HttpMethodExtensions on HttpMethod {
   String toMethodString() {
     switch (this) {
-      case HttpMethod.GET: return "GET";
-      case HttpMethod.POST: return "POST";
-      default: throw "Given HttpMethod is out of bounds.";
+      case HttpMethod.GET:
+        return "GET";
+      case HttpMethod.POST:
+        return "POST";
+      case HttpMethod.PUT:
+        return "PUT";
+      case HttpMethod.DELETE:
+        return "DELETE";
+      default:
+        throw "Given HttpMethod is out of bounds.";
     }
   }
 }
