0247d1bd1f752f67c6bfa0fd57ac1d5e237ac404
Craftplacer <craftplacer@outlook.de>
2020-11-17 01:43:04 +0100

    Add groupBy iterable extension method

--- src/kaiteki/lib/utils/extensions/iterable.dart
+++ src/kaiteki/lib/utils/extensions/iterable.dart
@@ -5,4 +5,20 @@ extension IterableExtensions<T> on Iterable<T> {
     }
     return null;
   }
+
+  Map<K, Iterable<T>> groupBy<K>(K groupingFunction(T element)) {
+    var map = Map<K, List<T>>();
+
+    for (T element in this) {
+      var key = groupingFunction(element);
+
+      if (!map.containsKey(key)) {
+        map[key] = <T>[];
+      }
+
+      map[key].add(element);
+    }
+
+    return map;
+  }
 }
