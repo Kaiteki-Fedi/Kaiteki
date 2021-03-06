764d80308a6a8daa5b53899e4a17786be742e936
Craftplacer <craftplacer@outlook.de>
2021-03-06 16:53:28 +0100

    Refactor StatusWidget

--- src/kaiteki/lib/ui/widgets/status_widget.dart
+++ src/kaiteki/lib/ui/widgets/status_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:kaiteki/model/fediverse/attachment.dart';
 import 'package:kaiteki/model/fediverse/post.dart';
 import 'package:kaiteki/model/fediverse/visibility.dart';
+import 'package:kaiteki/theming/app_theme.dart';
 import 'package:kaiteki/theming/theme_container.dart';
 import 'package:kaiteki/ui/widgets/attachments/image_attachment_widget.dart';
 import 'package:kaiteki/ui/widgets/posts/avatar_widget.dart';
@@ -12,14 +13,16 @@ import 'package:kaiteki/ui/widgets/posts/reaction_row.dart';
 import 'package:kaiteki/utils/extensions/duration.dart';
 import 'package:kaiteki/utils/text/text_renderer.dart';
 import 'package:kaiteki/utils/text/text_renderer_theme.dart';
+import 'package:kaiteki/utils/utils.dart';
 import 'package:mdi/mdi.dart';
 import 'package:provider/provider.dart';
 
 class StatusWidget extends StatelessWidget {
   final Post _post;
   final VoidCallback onTap;
+  final bool showParentPost;
 
-  const StatusWidget(this._post, {this.onTap});
+  const StatusWidget(this._post, {this.onTap, this.showParentPost = true});
 
   @override
   Widget build(BuildContext context) {
@@ -74,37 +77,18 @@ class StatusWidget extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Row(
-                      children: [
-                        if (renderedAuthor != null)
-                          RichText(text: renderedAuthor),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 6),
-                          child: Text(
-                            _post.author.username,
-                            style: TextStyle(
-                              color: theme.materialTheme.disabledColor,
-                            ),
-                          ),
-                        ),
-                        Spacer(),
-                        Text(
-                          DateTime.now()
-                              .difference(_post.postedAt)
-                              .toStringHuman(),
-                          style: TextStyle(
-                            color: theme.materialTheme.disabledColor,
-                          ),
-                        ),
-                        if (_post.visibility != null)
-                          Icon(
-                            _post.visibility.toIconData(),
-                            size: 20,
-                            color: theme.materialTheme.disabledColor,
-                          ),
-                      ],
+                    MetaBar(
+                      renderedAuthor: renderedAuthor,
+                      post: _post,
+                      theme: theme,
                     ),
-                    if (renderedContent != null) RichText(text: renderedContent),
+
+                    if (showParentPost && _post.replyToPostId != null)
+                      ReplyBar(textStyle: textStyle, post: _post),
+
+                    if (renderedContent != null)
+                      RichText(text: renderedContent),
+
                     if (_post.attachments != null)
                       AttachmentRow(
                         attachments: _post.attachments.toList(growable: false),
@@ -119,35 +103,7 @@ class StatusWidget extends StatelessWidget {
                     if (_post.reactions != null)
                       ReactionRow(_post, _post.reactions),
 
-                    Row(
-                      children: [
-                        CountButton(
-                          icon: Icon(Icons.reply),
-                          count: _post.replyCount,
-                        ),
-                        CountButton(
-                          icon: Icon(Icons.repeat),
-                          count: _post.repeatCount,
-                          active: _post.repeated,
-                          activeColor: theme.repeatColor,
-                        ),
-                        CountButton(
-                          icon: Icon(Mdi.starOutline),
-                          count: _post.likeCount,
-                          active: _post.liked,
-                          activeColor: theme.favoriteColor,
-                          activeIcon: Icon(Icons.star),
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.insert_emoticon),
-                          onPressed: null,
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.more_horiz),
-                          onPressed: null,
-                        ),
-                      ],
-                    ),
+                    InteractionBar(post: _post, theme: theme),
                     // ApplicationWidget(_post.application),
                   ],
                 ),
@@ -156,6 +112,135 @@ class StatusWidget extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class MetaBar extends StatelessWidget {
+  const MetaBar({
+    Key key,
+    @required this.renderedAuthor,
+    @required Post post,
+    @required this.theme,
+  })  : _post = post,
+        super(key: key);
+
+  final InlineSpan renderedAuthor;
+  final Post _post;
+  final AppTheme theme;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        if (renderedAuthor != null) RichText(text: renderedAuthor),
+        Padding(
+          padding: const EdgeInsets.only(left: 6),
+          child: Text(
+            _post.author.username,
+            style: TextStyle(
+              color: theme.materialTheme.disabledColor,
+            ),
+          ),
+        ),
+        Spacer(),
+        Text(
+          DateTime.now().difference(_post.postedAt).toStringHuman(),
+          style: TextStyle(
+            color: theme.materialTheme.disabledColor,
+          ),
+        ),
+        if (_post.visibility != null)
+          Icon(
+            _post.visibility.toIconData(),
+            size: 20,
+            color: theme.materialTheme.disabledColor,
+          ),
+      ],
+    );
+  }
+}
+
+class ReplyBar extends StatelessWidget {
+  const ReplyBar({
+    Key key,
+    @required this.textStyle,
+    @required Post post,
+  })  : _post = post,
+        super(key: key);
+
+  final TextStyle textStyle;
+  final Post _post;
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        style: textStyle,
+        children: [
+          // TODO: refactor the following widget pattern to a future "IconSpan"
+          WidgetSpan(
+            child: Icon(
+              Icons.reply,
+              size: Utils.getLocalFontSize(context) * 1.25,
+              color: Theme.of(context).disabledColor,
+            ),
+          ),
+          TextSpan(
+            text: " Reply to ",
+            style: TextStyle(color: Theme.of(context).disabledColor),
+          ),
+          TextSpan(
+            text: _post.replyToAccountId,
+            style: TextStyle(color: Theme.of(context).accentColor),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class InteractionBar extends StatelessWidget {
+  const InteractionBar({
+    Key key,
+    @required Post post,
+    @required this.theme,
+  })  : _post = post,
+        super(key: key);
+
+  final Post _post;
+  final AppTheme theme;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CountButton(
+          icon: Icon(Icons.reply),
+          count: _post.replyCount,
+        ),
+        CountButton(
+          icon: Icon(Icons.repeat),
+          count: _post.repeatCount,
+          active: _post.repeated,
+          activeColor: theme.repeatColor,
+        ),
+        CountButton(
+          icon: Icon(Mdi.starOutline),
+          count: _post.likeCount,
+          active: _post.liked,
+          activeColor: theme.favoriteColor,
+          activeIcon: Icon(Icons.star),
+        ),
+        IconButton(
+          icon: Icon(Icons.insert_emoticon),
+          onPressed: null,
+        ),
+        IconButton(
+          icon: Icon(Icons.more_horiz),
+          onPressed: null,
+        ),
+      ],
     );
   }
 }
