ab842a5e9cf2dadc9511c66cfce5ebc79307ddf5
Craftplacer <craftplacer@craftplacer.moe>
2021-07-03 17:53:25 +0200

    Add nullTransform helper function

--- src/kaiteki/lib/utils/extensions.dart
+++ src/kaiteki/lib/utils/extensions.dart
@@ -1,3 +1,11 @@
 export 'package:kaiteki/utils/extensions/duration.dart';
 export 'package:kaiteki/utils/extensions/iterable.dart';
 export 'package:kaiteki/utils/extensions/string.dart';
+
+extension ObjectExtensions on Object? {
+  TO? nullTransform<TI, TO>(TO Function(TI object) function) {
+    if (this == null) return null;
+
+    return function.call(this! as TI);
+  }
+}
