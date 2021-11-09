9495aeb70d06cd83e45d429ee5473b6009ba64aa
Craftplacer <craftplacer@craftplacer.moe>
2021-11-09 10:48:15 +0100

    Remove "impossible" message for `TimelinePage`

--- src/kaiteki/lib/ui/pages/timeline_page.dart
+++ src/kaiteki/lib/ui/pages/timeline_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kaiteki/account_manager.dart';
 import 'package:kaiteki/model/post_filters/sensitive_post_filter.dart';
-import 'package:kaiteki/ui/widgets/icon_landing_widget.dart';
 import 'package:kaiteki/ui/widgets/timeline.dart';
-import 'package:mdi/mdi.dart';
 import 'package:provider/provider.dart';
 
 class TimelinePage extends StatefulWidget {
@@ -18,15 +16,6 @@ class _TimelinePageState extends State<TimelinePage> {
   Widget build(BuildContext context) {
     var container = Provider.of<AccountManager>(context);
     var timelineKey = ValueKey(container.currentAccount.hashCode);
-
-    if (!container.loggedIn) {
-      return const Center(
-        child: IconLandingWidget(
-          icon: Icon(Mdi.key),
-          text: Text('You need to be signed in to view your timeline'),
-        ),
-      );
-    }
 
     return Center(
       child: ConstrainedBox(
