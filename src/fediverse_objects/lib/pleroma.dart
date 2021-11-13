e8427e0c24e9f4c26588b1555e6eaadde61b3f57
Craftplacer <craftplacer@craftplacer.moe>
2021-11-13 05:21:10 +0100

    Include emoji_reaction.dart inside pleroma.dart

--- src/fediverse_objects/lib/pleroma.dart
+++ src/fediverse_objects/lib/pleroma.dart
@@ -6,6 +6,7 @@ export 'src/pleroma/card.dart';
 export 'src/pleroma/chat.dart';
 export 'src/pleroma/chat_message.dart';
 export 'src/pleroma/emoji_pack.dart';
+export 'src/pleroma/emoji_reaction.dart';
 export 'src/pleroma/frontend_configuration.dart';
 export 'src/pleroma/pleroma_frontend_configuration.dart';
 export 'src/pleroma/status.dart';
