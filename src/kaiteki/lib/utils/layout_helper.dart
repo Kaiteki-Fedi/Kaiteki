268174ebcaccc38fd4386f2e0c96a9569310f3d5
Craftplacer <craftplacer@craftplacer.moe>
2022-01-08 01:32:44 +0100

    Implement responsive layout helpers

--- src/kaiteki/lib/utils/layout_helper.dart
+++ src/kaiteki/lib/utils/layout_helper.dart
@@ -1,28 +1,68 @@
-/// Returns a [ScreenSize] as defined by <https://material.io/design/layout/responsive-layout-grid.html#breakpoints>
+import 'package:flutter/widgets.dart';
+
+const int largeBreakpoint = 1440;
+const int mediumBreakpoint = 1240;
+const int smallBreakpoint1 = 600;
+const int smallBreakpoint2 = 905;
+
+/// Returns a [ScreenSize] based on the provided width.
+///
+/// Read more: <https://material.io/design/layout/responsive-layout-grid.html#breakpoints>
 ScreenSize getScreenSize(num width) {
-  if (width >= 1440) {
+  if (width >= largeBreakpoint) {
     return ScreenSize.l;
-  } else if (width >= 1240) {
+  } else if (width >= mediumBreakpoint) {
     return ScreenSize.m;
-  } else if (width >= 600) {
+  } else if (width >= smallBreakpoint1) {
     return ScreenSize.s;
   } else {
     return ScreenSize.xs;
   }
 }
 
-extension ScreenSizeExtensions on ScreenSize {
-  int toInt() {
-    switch (this) {
-      case ScreenSize.xs:
-        return 0;
-      case ScreenSize.s:
-        return 1;
-      case ScreenSize.m:
-        return 2;
-      case ScreenSize.l:
-        return 3;
-    }
+double? getBodySize(num width) {
+  if (width >= largeBreakpoint) {
+    return 1040;
+  } else if (width >= mediumBreakpoint) {
+    return null;
+  } else if (width >= smallBreakpoint2) {
+    return 840;
+  } else {
+    return null;
+  }
+}
+
+double? getMargin(num width) {
+  if (width >= largeBreakpoint) {
+    return null;
+  } else if (width >= mediumBreakpoint) {
+    return 200;
+  } else if (width >= smallBreakpoint2) {
+    return null;
+  } else if (width >= smallBreakpoint1) {
+    return 32;
+  } else {
+    return 16;
+  }
+}
+
+EdgeInsets getMarginPadding(num width) {
+  final margin = getMargin(width);
+
+  if (margin == null) {
+    return EdgeInsets.zero;
+  } else {
+    return EdgeInsets.symmetric(horizontal: margin);
+  }
+}
+
+double getColumns(num width) {
+  if (width >= smallBreakpoint2) {
+    return 12;
+  } else if (width >= smallBreakpoint1) {
+    return 8;
+  } else {
+    return 4;
   }
 }
 
@@ -38,4 +78,66 @@ enum ScreenSize {
 
   /// Large (desktop)
   l,
+}
+
+class ResponsiveLayoutBuilder extends StatelessWidget {
+  final bool center;
+  final Function(
+    BuildContext context,
+    BoxConstraints constraints,
+    ResponsiveLayoutData data,
+  ) builder;
+
+  const ResponsiveLayoutBuilder({
+    Key? key,
+    this.center = true,
+    required this.builder,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget widget = LayoutBuilder(builder: buildOnConstraints);
+
+    if (center) {
+      widget = Center(child: widget);
+    }
+
+    return widget;
+  }
+
+  Widget buildOnConstraints(context, constraints) {
+    final layout = ResponsiveLayoutData.width(constraints.maxWidth);
+    final margin = layout.margin;
+    final marginPadding = margin == null
+        ? EdgeInsets.zero
+        : EdgeInsets.symmetric(horizontal: margin);
+
+    return SizedBox(
+      width: layout.bodySize,
+      child: Padding(
+        padding: marginPadding,
+        child: builder(context, constraints, layout),
+      ),
+    );
+  }
+}
+
+class ResponsiveLayoutData {
+  final double? margin;
+  final double columns;
+  final double? bodySize;
+
+  const ResponsiveLayoutData({
+    required this.bodySize,
+    required this.margin,
+    required this.columns,
+  });
+
+  factory ResponsiveLayoutData.width(num width) {
+    return ResponsiveLayoutData(
+      bodySize: getBodySize(width),
+      margin: getMargin(width),
+      columns: getColumns(width),
+    );
+  }
 }
