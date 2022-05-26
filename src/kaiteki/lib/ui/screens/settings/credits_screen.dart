446b9a39f1b79e1bd62673a4fc1083a14269e33e
Craftplacer <craftplacer@craftplacer.moe>
2022-05-26 19:17:40 +0200

    Refactor credits to load from file
    Fixes #121

--- src/kaiteki/lib/ui/screens/settings/credits_screen.dart
+++ src/kaiteki/lib/ui/screens/settings/credits_screen.dart
@@ -1,25 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:json_annotation/json_annotation.dart';
 import 'package:kaiteki/di.dart';
+import 'package:kaiteki/ui/widgets/icon_landing_widget.dart';
 import 'package:kaiteki/utils/extensions.dart';
 import 'package:mdi/mdi.dart';
 
-// TODO(Craftplacer): Move credits to asset JSON file, https://github.com/Kaiteki-Fedi/Kaiteki/issues/121
-const _items = [
-  _CreditsEntry(
-    "Craftplacer",
-    "Maintainer, Translator (German)",
-    "https://craftplacer.moe/",
-  ),
-  _CreditsEntry("Odyssey98", "Icon design", "https://mstdn.social/@odyssey98"),
-  _CreditsEntry(
-    "NaiJi",
-    "Translator (Russian)",
-    "https://udongein.xyz/users/NaiJi",
-  ),
-];
+part 'credits_screen.g.dart';
 
-class CreditsScreen extends StatelessWidget {
+class CreditsScreen extends StatefulWidget {
   const CreditsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CreditsScreen> createState() => _CreditsScreenState();
+}
+
+class _CreditsScreenState extends State<CreditsScreen> {
+  late final Future<List<CreditsItem>> _future;
+
+  @override
+  void initState() {
+    super.initState();
+    _future = fetchCredits();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,30 +32,76 @@ class CreditsScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text(l10n.creditsTitle)),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final item = _items[index];
-          return ListTile(
-            title: Text(item.name),
-            subtitle: Text(item.role),
-            trailing: item.website.nullTransform(
-              (website) => IconButton(
-                icon: const Icon(Mdi.openInNew),
-                onPressed: () => context.launchUrl(item.website!),
+      body: FutureBuilder<List<CreditsItem>>(
+        future: _future,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: IconLandingWidget(
+                icon: Icon(Icons.error),
+                text: Text("Failed fetching credits"),
               ),
-            ),
-          );
+            );
+          } else if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            final items = snapshot.data!;
+            return ListView.builder(
+              itemBuilder: (_, i) => CreditsItemWidget(item: items[i]),
+              itemCount: items.length,
+            );
+          }
         },
-        itemCount: _items.length,
+      ),
+    );
+  }
+
+  Future<List<CreditsItem>> fetchCredits() async {
+    final json = await rootBundle.loadString('assets/credits.json');
+    return (jsonDecode(json) as List<dynamic>)
+        .cast<Map<String, dynamic>>()
+        .map(CreditsItem.fromJson)
+        .toList(growable: false);
+  }
+}
+
+class CreditsItemWidget extends StatelessWidget {
+  const CreditsItemWidget({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
+
+  final CreditsItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(item.name),
+      subtitle: Text(item.roles.join(", ")),
+      trailing: item.url.nullTransform(
+        (website) => IconButton(
+          icon: const Icon(Mdi.openInNew),
+          onPressed: () => context.launchUrl(item.url!),
+        ),
       ),
     );
   }
 }
 
-class _CreditsEntry {
+@JsonSerializable()
+class CreditsItem {
   final String name;
-  final String role;
-  final String? website;
+  final String? url;
+  final List<String> roles;
 
-  const _CreditsEntry(this.name, this.role, this.website);
+  CreditsItem({
+    required this.name,
+    this.url,
+    this.roles = const [],
+  });
+
+  factory CreditsItem.fromJson(Map<String, dynamic> json) =>
+      _$CreditsItemFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CreditsItemToJson(this);
 }
