599e9d1f53a2a82615319ad31a8c40bca07eeb01
Craftplacer <craftplacer@craftplacer.moe>
2022-05-27 12:46:07 +0200

    Remove unused `LoginCallback` typedef

--- src/kaiteki/lib/auth/login_typedefs.dart
+++ src/kaiteki/lib/auth/login_typedefs.dart
@@ -1,11 +1,2 @@
-import 'package:kaiteki/account_manager.dart';
-import 'package:kaiteki/model/auth/login_result.dart';
-
 typedef MfaCallback = Future<String?> Function();
-typedef LoginCallback = Future<LoginResult> Function(
-  String instance,
-  String username,
-  String password,
-  MfaCallback mfaCallback,
-  AccountManager accounts,
-);
+typedef OAuthCallback = Future<Map<String, String>> Function();
