4b0006cb9e858ef961c9a42dacd1fd341496d13b
Craftplacer <craftplacer@craftplacer.moe>
2022-05-30 23:54:38 +0200

    Update icons

--- render-icons.ps1
+++ render-icons.ps1
@@ -10,5 +10,11 @@ Write-Host "Exporting web PWA icons"
 inkscape --export-filename "src/kaiteki/web/icons/Icon-192.png" --export-type="png" -w 192 -h 192 "assets/icons/windows/kaiteki-small.svg" >$null
 inkscape --export-filename "src/kaiteki/web/icons/Icon-512.png" --export-type="png" -w 512 -h 512 "assets/icons/windows/kaiteki-small.svg" >$null
 
+Write-Host "Exporting macOS icons"
+Foreach ($size in 16,32,64,128,256,512,1024) {
+    inkscape --export-filename "src/kaiteki/macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_$size.png" --export-type="png" -w $size -h $size "assets/icons/windows/kaiteki-small.svg" >$null
+}
+
+
 Write-Host ""
 Write-Host "Please note that Android icons cannot be rendered over a script. Use Android Studio to generate Android icons."
