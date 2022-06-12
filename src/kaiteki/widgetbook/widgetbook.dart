ab8c94b7e135d819625b8239a0f82aa01445437d
Craftplacer <craftplacer@craftplacer.moe>
2022-06-12 10:38:41 +0200

    Update widgetbook

--- src/kaiteki/widgetbook/widgetbook.dart
+++ src/kaiteki/widgetbook/widgetbook.dart
@@ -1,17 +1,12 @@
-import 'package:flutter/material.dart' hide Visibility;
+import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:kaiteki/constants.dart' as consts;
-import 'package:kaiteki/fediverse/api_type.dart';
-import 'package:kaiteki/fediverse/model/post.dart';
-import 'package:kaiteki/fediverse/model/user.dart';
-import 'package:kaiteki/fediverse/model/visibility.dart';
 import 'package:kaiteki/theming/default_app_themes.dart';
-import 'package:kaiteki/ui/auth/login/api_type_dialog.dart';
-import 'package:kaiteki/ui/auth/login/api_web_compatibility_dialog.dart';
-import 'package:kaiteki/ui/shared/posts/post_widget.dart';
 import 'package:widgetbook/widgetbook.dart';
 
-import 'widgetboot_extensions.dart';
+import 'categories/dialogs.dart';
+import 'categories/posts.dart';
+import 'categories/user.dart';
 
 class HotReload extends StatelessWidget {
   const HotReload({Key? key}) : super(key: key);
@@ -24,145 +19,55 @@ class HotReload extends StatelessWidget {
       categories: [
         WidgetbookCategory(
           name: "Posts",
-          widgets: [
-            WidgetbookComponent(
-              name: "PostWidget",
-              useCases: [
-                WidgetbookUseCase(
-                  name: "Example post",
-                  builder: (context) {
-                    return Center(
-                      child: ConstrainedBox(
-                        constraints: const BoxConstraints(maxWidth: 500),
-                        child: PostWidget(
-                          Post.example(),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-                WidgetbookUseCase(
-                  name: "Customizable post",
-                  builder: (context) {
-                    final user = User(
-                      displayName: context.knobs.text(
-                        label: "Display name",
-                        initialValue: "User",
-                      ),
-                      host: context.knobs.text(
-                        label: "Instance",
-                        initialValue: "instance.social",
-                      ),
-                      id: '',
-                      avatarUrl: context.knobs.nullableText(
-                        label: "Avatar URL",
-                      ),
-                      source: null,
-                      username: context.knobs.text(
-                        label: "Username",
-                        initialValue: "User",
-                      ),
-                    );
-                    final post = Post(
-                      author: user,
-                      id: '',
-                      postedAt: DateTime.now().subtract(
-                        const Duration(minutes: 1),
-                      ),
-                      source: null,
-                      visibility: context.knobs.enumOptions(
-                        values: Visibility.values,
-                        label: "Visibility",
-                      ),
-                      content: context.knobs.nullableText(label: "Content"),
-                    );
-                    return Center(
-                      child: ConstrainedBox(
-                        constraints: const BoxConstraints(maxWidth: 500),
-                        child: PostWidget(post),
-                      ),
-                    );
-                  },
-                )
-              ],
-            )
-          ],
+          widgets: buildPostComponents(),
         ),
         WidgetbookCategory(
           name: "Dialogs",
-          widgets: [
-            WidgetbookComponent(
-              name: "ApiWebCompatibilityDialog",
-              useCases: [
-                WidgetbookUseCase(
-                  name: "Default",
-                  builder: (context) {
-                    return ColoredBox(
-                      color: Theme.of(context).colorScheme.surfaceVariant,
-                      child: Center(
-                        child: ApiWebCompatibilityDialog(
-                          type: context.knobs.enumOptions(
-                            label: "API type",
-                            values: ApiType.values,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-            WidgetbookComponent(
-              name: "ApiTypeDialog",
-              useCases: [
-                WidgetbookUseCase(
-                  name: "Default",
-                  builder: (context) {
-                    return ColoredBox(
-                      color: Theme.of(context).colorScheme.surfaceVariant,
-                      child: const Center(child: ApiTypeDialog()),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ],
+          widgets: buildDialogComponents(),
+        ),
+        WidgetbookCategory(
+          name: "Users",
+          widgets: buildUserComponents(),
         ),
       ],
       themes: [
         WidgetbookTheme(name: "Light", data: lightThemeData),
         WidgetbookTheme(name: "Dark", data: darkThemeData),
       ],
-      devices: [
-        Device(
-          name: 'Monitor',
-          resolution: Resolution.dimensions(
-            nativeWidth: 1920,
-            nativeHeight: 1080,
-            scaleFactor: 1,
-          ),
-          type: DeviceType.desktop,
-        ),
-        Device(
-          name: 'Laptop',
-          resolution: Resolution.dimensions(
-            nativeWidth: 1366,
-            nativeHeight: 768,
-            scaleFactor: 1,
-          ),
-          type: DeviceType.desktop,
-        ),
-        Device(
-          name: 'Samsung Galaxy Star 2 Plus',
-          resolution: Resolution.dimensions(
-            nativeWidth: 480,
-            nativeHeight: 800,
-            scaleFactor: 1,
-          ),
-          type: DeviceType.mobile,
-        ),
-      ],
+      devices: getDevices(),
       appInfo: AppInfo(name: consts.appName),
     );
+  }
+
+  List<Device> getDevices() {
+    return [
+      Device(
+        name: 'Monitor',
+        resolution: Resolution.dimensions(
+          nativeWidth: 1920,
+          nativeHeight: 1080,
+          scaleFactor: 1,
+        ),
+        type: DeviceType.desktop,
+      ),
+      Device(
+        name: 'Laptop',
+        resolution: Resolution.dimensions(
+          nativeWidth: 1366,
+          nativeHeight: 768,
+          scaleFactor: 1,
+        ),
+        type: DeviceType.desktop,
+      ),
+      Device(
+        name: 'Samsung Galaxy Star 2 Plus',
+        resolution: Resolution.dimensions(
+          nativeWidth: 480,
+          nativeHeight: 800,
+          scaleFactor: 1,
+        ),
+        type: DeviceType.mobile,
+      ),
+    ];
   }
 }
