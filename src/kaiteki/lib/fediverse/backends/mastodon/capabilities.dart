1f12d622b64543e2457ba3b7eefbf0acec5447c7
Craftplacer <craftplacer@craftplacer.moe>
2022-06-23 23:33:49 +0200

    Add `supportsSubjects` to `AdapterCapabilities`

--- src/kaiteki/lib/fediverse/backends/mastodon/capabilities.dart
+++ src/kaiteki/lib/fediverse/backends/mastodon/capabilities.dart
@@ -2,10 +2,13 @@ import 'package:kaiteki/fediverse/capabilities.dart';
 import 'package:kaiteki/fediverse/model/formatting.dart';
 
 class MastodonCapabilities extends AdapterCapabilities {
+  const MastodonCapabilities();
+
   @override
   bool get supportsScopes => true;
 
-  const MastodonCapabilities();
+  @override
+  bool get supportsSubjects => true;
 
   @override
   List<Formatting> get supportedFormattings {
