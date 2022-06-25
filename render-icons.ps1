05fa2be9d9f871462d1d0d6f647256412bd1253b
Craftplacer <craftplacer@craftplacer.moe>
2022-06-25 11:16:20 +0200

    Use `Write-Output` instead of `Write-Host`

--- render-icons.ps1
+++ render-icons.ps1
@@ -3,18 +3,17 @@ If (-Not (Get-Command "inkscape" -ErrorAction SilentlyContinue)) {
     Throw "Couldn't find inkscape"
 }
 
-Write-Host "Exporting web favicon"
+Write-Output "Exporting web favicon"
 inkscape --export-filename "src/kaiteki/web/favicon.png" --export-type="png" "assets/icons/windows/kaiteki-small.svg" >$null
 
-Write-Host "Exporting web PWA icons"
+Write-Output "Exporting web PWA icons"
 inkscape --export-filename "src/kaiteki/web/icons/Icon-192.png" --export-type="png" -w 192 -h 192 "assets/icons/windows/kaiteki-small.svg" >$null
 inkscape --export-filename "src/kaiteki/web/icons/Icon-512.png" --export-type="png" -w 512 -h 512 "assets/icons/windows/kaiteki-small.svg" >$null
 
-Write-Host "Exporting macOS icons"
+Write-Output "Exporting macOS icons"
 Foreach ($size in 16,32,64,128,256,512,1024) {
     inkscape --export-filename "src/kaiteki/macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_$size.png" --export-type="png" -w $size -h $size "assets/icons/windows/kaiteki-small.svg" >$null
 }
 
-
-Write-Host ""
-Write-Host "Please note that Android icons cannot be rendered over a script. Use Android Studio to generate Android icons."
+Write-Output ""
+Write-Output "Please note that Android icons cannot be rendered over a script. Use Android Studio to generate Android icons."
