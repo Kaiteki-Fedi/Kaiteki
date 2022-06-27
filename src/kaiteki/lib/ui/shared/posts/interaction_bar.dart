692fc38ec28b7a0e69f213b8d9fdadade0d7315c
Craftplacer <craftplacer@craftplacer.moe>
2022-06-27 14:48:30 +0200

    Reduce redundant lines in `interaction_bar.dart`

--- src/kaiteki/lib/ui/shared/posts/interaction_bar.dart
+++ src/kaiteki/lib/ui/shared/posts/interaction_bar.dart
@@ -21,107 +21,77 @@ class InteractionBar extends StatelessWidget {
     final l10n = context.getL10n();
     final kTheme = context.kaitekiExtension!;
 
+    final buttons = [
+      CountButton(
+        icon: const Icon(Icons.reply_rounded),
+        count: _post.replyCount,
+        onTap: () => context.showPostDialog(replyTo: _post),
+        focusNode: FocusNode(skipTraversal: true),
+      ),
+      CountButton(
+        icon: const Icon(Icons.repeat_rounded),
+        count: _post.repeatCount,
+        active: _post.repeated,
+        activeColor: kTheme.repeatColor,
+        focusNode: FocusNode(skipTraversal: true),
+      ),
+      CountButton(
+        icon: const Icon(Icons.star_border_rounded),
+        count: _post.likeCount,
+        active: _post.liked,
+        activeColor: kTheme.favoriteColor,
+        activeIcon: const Icon(Icons.star_rounded),
+        focusNode: FocusNode(skipTraversal: true),
+      ),
+      IconButton(
+        icon: const Icon(Icons.mood_rounded),
+        onPressed: null,
+        focusNode: FocusNode(skipTraversal: true),
+      ),
+      PopupMenuButton<VoidCallback>(
+        icon: const Icon(Icons.more_horiz),
+        onSelected: (callback) => callback.call(),
+        itemBuilder: (context) {
+          return [
+            PopupMenuItem(
+              enabled: openInBrowserAvailable,
+              child: ListTile(
+                title: Text(l10n.openInBrowserLabel),
+                leading: const Icon(Icons.open_in_new_rounded),
+                contentPadding: EdgeInsets.zero,
+                enabled: openInBrowserAvailable,
+              ),
+              value: () => context.launchUrl(_post.externalUrl!),
+            ),
+            if (_post.content != null)
+              PopupMenuItem(
+                child: const ListTile(
+                  title: Text("Debug text rendering"),
+                  leading: Icon(Icons.bug_report_rounded),
+                  contentPadding: EdgeInsets.zero,
+                ),
+                value: () => showDialog(
+                  context: context,
+                  builder: (context) => TextRenderDialog(_post),
+                ),
+              ),
+          ];
+        },
+      ),
+    ];
+
     // Added Material for fixing bork with Hero *shrug*
     return ConstrainedBox(
-      constraints: const BoxConstraints(
-        maxWidth: 450,
-      ),
+      constraints: const BoxConstraints(maxWidth: 450),
       child: Flex(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         direction: Axis.horizontal,
         children: [
-          Flexible(
-            fit: FlexFit.tight,
-            child: Row(
-              children: [
-                CountButton(
-                  icon: const Icon(Icons.reply_rounded),
-                  count: _post.replyCount,
-                  buttonOnly: true,
-                  onTap: () => context.showPostDialog(replyTo: _post),
-                  focusNode: FocusNode(skipTraversal: true),
-                ),
-              ],
+          for (var button in buttons)
+            Flexible(
+              fit: FlexFit.tight,
+              child: Row(children: [button]),
             ),
-          ),
-          Flexible(
-            fit: FlexFit.tight,
-            child: Row(
-              children: [
-                CountButton(
-                  icon: const Icon(Icons.repeat_rounded),
-                  count: _post.repeatCount,
-                  active: _post.repeated,
-                  activeColor: kTheme.repeatColor,
-                  focusNode: FocusNode(skipTraversal: true),
-                ),
-              ],
-            ),
-          ),
-          Flexible(
-            fit: FlexFit.tight,
-            child: Row(
-              children: [
-                CountButton(
-                  icon: const Icon(Icons.star_border_rounded),
-                  count: _post.likeCount,
-                  active: _post.liked,
-                  activeColor: kTheme.favoriteColor,
-                  activeIcon: const Icon(Icons.star_rounded),
-                  focusNode: FocusNode(skipTraversal: true),
-                ),
-              ],
-            ),
-          ),
-          Flexible(
-            fit: FlexFit.tight,
-            child: Row(
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.mood_rounded),
-                  onPressed: null,
-                  focusNode: FocusNode(skipTraversal: true),
-                ),
-              ],
-            ),
-          ),
-          Flexible(
-            fit: FlexFit.tight,
-            child: Row(
-              children: [
-                PopupMenuButton<VoidCallback>(
-                  icon: const Icon(Icons.more_horiz),
-                  onSelected: (callback) => callback.call(),
-                  itemBuilder: (context) {
-                    return [
-                      PopupMenuItem(
-                        enabled: openInBrowserAvailable,
-                        child: ListTile(
-                          title: Text(l10n.openInBrowserLabel),
-                          leading: const Icon(Icons.open_in_new_rounded),
-                          contentPadding: EdgeInsets.zero,
-                          enabled: openInBrowserAvailable,
-                        ),
-                        value: () => context.launchUrl(_post.externalUrl!),
-                      ),
-                      if (_post.content != null)
-                        PopupMenuItem(
-                          child: const ListTile(
-                            title: Text("Debug text rendering"),
-                            leading: Icon(Icons.bug_report_rounded),
-                            contentPadding: EdgeInsets.zero,
-                          ),
-                          value: () => showDialog(
-                            context: context,
-                            builder: (context) => TextRenderDialog(_post),
-                          ),
-                        ),
-                    ];
-                  },
-                ),
-              ],
-            ),
-          ),
         ],
       ),
     );
