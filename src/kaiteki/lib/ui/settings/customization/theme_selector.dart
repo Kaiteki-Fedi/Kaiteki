7ee868c717dcff3959c54e692d7e42c71fff42b0
Craftplacer <craftplacer@craftplacer.moe>
2022-06-30 20:16:04 +0200

    Update theme_selector.dart

--- src/kaiteki/lib/ui/settings/customization/theme_selector.dart
+++ src/kaiteki/lib/ui/settings/customization/theme_selector.dart
@@ -1,7 +1,7 @@
 // TODO(Craftplacer): Clean up and refactor these widgets to be more flexible
 import 'package:flutter/material.dart';
 import 'package:kaiteki/di.dart';
-import 'package:kaiteki/theming/default_app_themes.dart';
+import 'package:kaiteki/theming/default/themes.dart';
 import 'package:kaiteki/ui/settings/customization/theme_preview.dart';
 
 class ThemeSelector extends StatelessWidget {
