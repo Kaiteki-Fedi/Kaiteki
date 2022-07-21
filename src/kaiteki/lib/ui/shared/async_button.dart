1304de4a6e096fdb23ae7477b504cc4d38b52bf9
Craftplacer <craftplacer@craftplacer.moe>
2022-07-21 10:07:57 +0200

    Update async_button.dart

--- src/kaiteki/lib/ui/shared/async_button.dart
+++ src/kaiteki/lib/ui/shared/async_button.dart
@@ -6,11 +6,14 @@ import 'package:flutter/material.dart';
 class AsyncButton extends StatefulWidget {
   final Future Function() onPressed;
   final Widget child;
+  final AsyncButtonType type;
 
   const AsyncButton({
     super.key,
     required this.onPressed,
     required this.child,
+    // TODO(Craftplacer): Make AsyncButtonType part of theme
+    this.type = AsyncButtonType.leftCircular,
   });
 
   @override
@@ -20,54 +23,33 @@ class AsyncButton extends StatefulWidget {
 class _AsyncButtonState extends State<AsyncButton> {
   Future? _future;
 
+  bool get _isFutureRunning => _future != null;
+
   @override
   Widget build(BuildContext context) {
-    final isFutureRunning = _future != null;
-    final button = ElevatedButton(
-      onPressed: isFutureRunning ? null : _onPressed,
-      child: widget.child,
+    switch (widget.type) {
+      case AsyncButtonType.replace:
+        return buildReplace(context);
+      case AsyncButtonType.bottomLinear:
+        return buildBottomLinear(context);
+      case AsyncButtonType.leftCircular:
+        return buildLeftCircular(context);
+    }
+  }
+
+  Widget buildReplace(BuildContext context) {
+    const progressIndicator = SizedBox.square(
+      dimension: 18,
+      child: CircularProgressIndicator(strokeWidth: 3),
     );
 
-    return Stack(
-      children: [
-        button,
-        if (isFutureRunning)
-          Positioned.fill(
-            child: ClipPath(
-              clipper: _ButtonClipper(
-                button.defaultStyleOf(context).shape?.resolve({}),
-              ),
-              child: const Align(
-                alignment: Alignment.bottomCenter,
-                child: LinearProgressIndicator(
-                  backgroundColor: Colors.transparent,
-                ),
-              ),
-            ),
-          ),
-      ],
+    return ElevatedButton(
+      onPressed: _isFutureRunning ? null : _onPressed,
+      style: _isFutureRunning
+          ? ElevatedButton.styleFrom(padding: EdgeInsets.zero)
+          : null,
+      child: _isFutureRunning ? progressIndicator : widget.child,
     );
-
-    // if (_future == null) {
-    //   return ElevatedButton(
-    //     onPressed: _onPressed,
-    //     child: widget.child,
-    //   );
-    // }
-    //
-    // return ElevatedButton.icon(
-    //   icon: const Padding(
-    //     padding: EdgeInsets.only(right: 4.0),
-    //     child: SizedBox.square(
-    //       dimension: 18,
-    //       child: CircularProgressIndicator(
-    //         strokeWidth: 3,
-    //       ),
-    //     ),
-    //   ),
-    //   onPressed: null,
-    //   label: widget.child,
-    // );
   }
 
   Future<void> _onPressed() async {
@@ -85,6 +67,64 @@ class _AsyncButtonState extends State<AsyncButton> {
     // Update widget once Future has completed
     await future.then((_) => setState(() => _future = null));
   }
+
+  Widget buildLeftCircular(BuildContext context) {
+    return ElevatedButton(
+      onPressed: _isFutureRunning ? null : _onPressed,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (_isFutureRunning)
+            const Padding(
+              padding: EdgeInsets.only(right: 12),
+              child: SizedBox.square(
+                dimension: 16,
+                child: CircularProgressIndicator(strokeWidth: 3),
+              ),
+            ),
+          widget.child,
+        ],
+      ),
+    );
+  }
+
+  Widget buildBottomLinear(BuildContext context) {
+    final button = ElevatedButton(
+      onPressed: _isFutureRunning ? null : _onPressed,
+      child: widget.child,
+    );
+
+    return Stack(
+      children: [
+        button,
+        if (_isFutureRunning)
+          Positioned.fill(
+            child: ClipPath(
+              clipper: _ButtonClipper(
+                button.defaultStyleOf(context).shape?.resolve({}),
+              ),
+              child: const Align(
+                alignment: Alignment.bottomCenter,
+                child: LinearProgressIndicator(
+                  backgroundColor: Colors.transparent,
+                ),
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+}
+
+enum AsyncButtonType {
+  /// Replaces the child with a [CircularProgressIndicator].
+  replace,
+
+  /// Overlays a [LinearProgressIndicator] on the child.
+  bottomLinear,
+
+  /// Prepends the child with a [CircularProgressIndicator].
+  leftCircular
 }
 
 class _ButtonClipper extends CustomClipper<Path> {
