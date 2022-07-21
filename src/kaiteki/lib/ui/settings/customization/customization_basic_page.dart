36e4ecd85fc98eece1ca652ff22a4f30be69de86
Craftplacer <craftplacer@craftplacer.moe>
2022-07-21 10:07:35 +0200

    Add system default notice to theme settings

--- src/kaiteki/lib/ui/settings/customization/customization_basic_page.dart
+++ src/kaiteki/lib/ui/settings/customization/customization_basic_page.dart
@@ -1,6 +1,8 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:kaiteki/di.dart';
 import 'package:kaiteki/ui/settings/customization/theme_selector.dart';
+import 'package:kaiteki/ui/shared/text_inherited_icon_theme.dart';
 
 class CustomizationBasicPage extends ConsumerStatefulWidget {
   const CustomizationBasicPage({Key? key}) : super(key: key);
@@ -21,6 +23,10 @@ class _CustomizationBasicPageState
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: _SystemSettingsDisclaimer(),
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(
               horizontal: 20.0,
@@ -46,6 +52,61 @@ class _CustomizationBasicPageState
             onChanged: (value) => setState(() => prefs.useMaterial3 = value),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _SystemSettingsDisclaimer extends StatelessWidget {
+  const _SystemSettingsDisclaimer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Colors.lightBlueAccent.harmonizeWith(
+      Theme.of(context).colorScheme.primary,
+    );
+    final localFontSize = DefaultTextStyle.of(context).style.fontSize;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(4.0),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: color.withOpacity(.25),
+          border: BorderDirectional(
+            start: BorderSide(color: color, width: 4.0),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(Icons.info_rounded, color: color),
+              const SizedBox(width: 8.0),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(top: localFontSize! * 0.125),
+                  child: const Text.rich(
+                    TextSpan(
+                      text:
+                          "Some settings are automatically provided by your system and marked as indeterminate (",
+                      children: [
+                        WidgetSpan(
+                          child: TextInheritedIconTheme(
+                            child: Icon(Icons.indeterminate_check_box_rounded),
+                          ),
+                          alignment: PlaceholderAlignment.middle,
+                          baseline: TextBaseline.alphabetic,
+                        ),
+                        TextSpan(text: ") by default."),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
