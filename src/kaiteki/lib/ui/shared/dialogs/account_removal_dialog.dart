c44ef52a0e4623d29170bea5d4ff6bddd218da80
Craftplacer <craftplacer@craftplacer.moe>
2022-08-14 13:49:16 +0200

    Show account being removed in dialog

--- src/kaiteki/lib/ui/shared/dialogs/account_removal_dialog.dart
+++ src/kaiteki/lib/ui/shared/dialogs/account_removal_dialog.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:kaiteki/constants.dart' show dialogConstraints;
 import 'package:kaiteki/di.dart';
+import 'package:kaiteki/model/auth/account_compound.dart';
 import 'package:kaiteki/ui/shared/dialogs/dialog_title_with_hero.dart';
+import 'package:kaiteki/ui/shared/posts/avatar_widget.dart';
+import 'package:kaiteki/utils/extensions.dart';
 
 class AccountRemovalDialog extends StatelessWidget {
-  const AccountRemovalDialog({super.key});
+  final Account? account;
+
+  const AccountRemovalDialog({super.key, this.account});
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +22,28 @@ class AccountRemovalDialog extends StatelessWidget {
           icon: const Icon(Icons.logout_rounded),
           title: Text(l10n.accountRemovalConfirmationTitle),
         ),
-        content: Text(l10n.accountRemovalConfirmationDescription),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(l10n.accountRemovalConfirmationDescription),
+            if (account != null)
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Consumer(
+                  builder: (context, ref, _) {
+                    return ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      title: Text.rich(
+                        account!.user.renderDisplayName(context, ref),
+                      ),
+                      subtitle: Text(account!.key.host),
+                      leading: AvatarWidget(account!.user),
+                    );
+                  },
+                ),
+              )
+          ],
+        ),
         actions: <Widget>[
           TextButton(
             onPressed: () => Navigator.of(context).pop(),
