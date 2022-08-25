6448611043105278a48de548992f82a379615e34
Craftplacer <craftplacer@craftplacer.moe>
2022-08-25 11:45:11 +0200

    Don't show Drawer under status bar

--- src/kaiteki/lib/ui/main/drawer.dart
+++ src/kaiteki/lib/ui/main/drawer.dart
@@ -13,66 +13,68 @@ class MainScreenDrawer extends ConsumerWidget {
     final account = ref.watch(accountProvider).currentAccount;
     final fontSize = Theme.of(context).textTheme.titleLarge?.fontSize;
     return Drawer(
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 18.0,
-                vertical: 16.0,
+      child: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 18.0,
+                  vertical: 16.0,
+                ),
+                child: Text(
+                  appName,
+                  style: Theme.of(context) //
+                      .ktkTextTheme
+                      ?.kaitekiTextStyle
+                      .copyWith(fontSize: fontSize),
+                ),
               ),
-              child: Text(
-                appName,
-                style: Theme.of(context) //
-                    .ktkTextTheme
-                    ?.kaitekiTextStyle
-                    .copyWith(fontSize: fontSize),
+              const ListTile(
+                leading: Icon(Icons.mail_rounded),
+                title: Text("Direct Messages"),
+                enabled: false,
               ),
-            ),
-            const ListTile(
-              leading: Icon(Icons.mail_rounded),
-              title: Text("Direct Messages"),
-              enabled: false,
-            ),
-            const ListTile(
-              leading: Icon(Icons.article_rounded),
-              title: Text("Lists"),
-              enabled: false,
-            ),
-            const ListTile(
-              leading: Icon(Icons.trending_up_rounded),
-              title: Text("Trends"),
-              enabled: false,
-            ),
-            ListTile(
-              leading: const Icon(Icons.flag_rounded),
-              title: Text(l10n.reportsTitle),
-              enabled: false,
-            ),
-            const Divider(),
-            ListTile(
-              title: Text("@${account.key.username}@${account.key.host}"),
-              enabled: false,
-            ),
-            ListTile(
-              leading: const Icon(Icons.manage_accounts_rounded),
-              title: Text(l10n.accountSettingsTitle),
-              // onTap: () => context.push("/$handle/settings"),
-              enabled: false,
-            ),
-            const Divider(),
-            ListTile(
-              leading: const Icon(Icons.settings_rounded),
-              title: Text(l10n.settings),
-              onTap: () => context.push("/settings"),
-            ),
-            ListTile(
-              leading: const Icon(Icons.info_outline_rounded),
-              title: Text(l10n.settingsAbout),
-              onTap: () => context.push("/about"),
-            ),
-          ],
+              const ListTile(
+                leading: Icon(Icons.article_rounded),
+                title: Text("Lists"),
+                enabled: false,
+              ),
+              const ListTile(
+                leading: Icon(Icons.trending_up_rounded),
+                title: Text("Trends"),
+                enabled: false,
+              ),
+              ListTile(
+                leading: const Icon(Icons.flag_rounded),
+                title: Text(l10n.reportsTitle),
+                enabled: false,
+              ),
+              const Divider(),
+              ListTile(
+                title: Text("@${account.key.username}@${account.key.host}"),
+                enabled: false,
+              ),
+              ListTile(
+                leading: const Icon(Icons.manage_accounts_rounded),
+                title: Text(l10n.accountSettingsTitle),
+                // onTap: () => context.push("/$handle/settings"),
+                enabled: false,
+              ),
+              const Divider(),
+              ListTile(
+                leading: const Icon(Icons.settings_rounded),
+                title: Text(l10n.settings),
+                onTap: () => context.push("/settings"),
+              ),
+              ListTile(
+                leading: const Icon(Icons.info_outline_rounded),
+                title: Text(l10n.settingsAbout),
+                onTap: () => context.push("/about"),
+              ),
+            ],
+          ),
         ),
       ),
     );
