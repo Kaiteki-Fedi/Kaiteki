a4fe5a6353b7a9f57786b9e5555a44f4361cdeb2
Craftplacer <craftplacer@craftplacer.moe>
2023-01-07 12:47:38 +0100

    Other things

--- src/kaiteki/lib/platform_checks.dart
+++ src/kaiteki/lib/platform_checks.dart
@@ -2,8 +2,17 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 
-bool get supportsVideoPlayer => kIsWeb || Platform.isAndroid || Platform.isIOS;
+bool get supportsVideoPlayer => kIsWeb || _isMobileOS;
 
 const kBuildFlavor = bool.hasEnvironment("BUILD_FLAVOR")
     ? String.fromEnvironment("BUILD_FLAVOR")
     : null;
+
+// share_plus
+bool get supportsDedicatedSharing =>
+    kIsWeb || !(Platform.isWindows || Platform.isLinux);
+
+// HACK(Craftplacer): I'd prefer an actual system call to check whether a hard keyboard is attached
+bool get hasPhysicalKeyboard => kIsWeb || !_isMobileOS;
+
+bool get _isMobileOS => Platform.isAndroid || Platform.isIOS;
