0e2998e773e7c1b2efa16847ce8ee91fd83151c3
Craftplacer <craftplacer@craftplacer.moe>
2023-01-07 18:01:56 +0100

    Add story for avatar blurhashes

--- src/kaiteki/storybook/main.dart
+++ src/kaiteki/storybook/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:kaiteki/fediverse/model/mock_data.dart';
+import 'package:kaiteki/fediverse/model/user/user.dart';
 import 'package:kaiteki/theming/default/themes.dart';
+import 'package:kaiteki/ui/shared/posts/avatar_widget.dart';
 import 'package:kaiteki/ui/shared/users/user_badge.dart';
 import 'package:storybook_flutter/storybook_flutter.dart';
 
@@ -35,6 +38,7 @@ class KaitekiStorybook extends StatelessWidget {
         keyboardShortcuts,
         userBadges,
         poll,
+        avatars,
       ],
     );
   }
@@ -51,5 +55,19 @@ final userBadges = Story(
       SizedBox(width: 8),
       AdministratorUserBadge(),
     ],
+  ),
+);
+
+final avatars = Story(
+  name: "Avatars",
+  builder: (_) => const AvatarWidget(
+    User(
+      username: "",
+      id: "",
+      host: "somewhere",
+      displayName: null,
+      avatarUrl: exampleAvatar,
+      avatarBlurHash: "UgQ9[\$WB~Xt7?uoyIUWVxbWBkBoLf+aeoeae",
+    ),
   ),
 );
