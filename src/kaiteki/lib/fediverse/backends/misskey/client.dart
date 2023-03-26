7a3db0f9fd075291e11144e6c2e768c3b8307214
Emerald <emerald_actual@proton.me>
2023-03-26 19:06:12 -0400

    list mutes from misskey

--- src/kaiteki/lib/fediverse/backends/misskey/client.dart
+++ src/kaiteki/lib/fediverse/backends/misskey/client.dart
@@ -1,6 +1,7 @@
 import "dart:async";
 import "dart:convert";
 import "dart:developer";
+import "dart:io";
 
 import "package:fediverse_objects/misskey.dart" as misskey;
 import "package:http/http.dart"
@@ -9,6 +10,7 @@ import "package:kaiteki/exceptions/http_exception.dart";
 import "package:kaiteki/fediverse/backends/misskey/exception.dart";
 import "package:kaiteki/fediverse/backends/misskey/model/follow.dart";
 import "package:kaiteki/fediverse/backends/misskey/model/list.dart";
+import "package:kaiteki/fediverse/backends/misskey/model/mute.dart";
 import "package:kaiteki/fediverse/backends/misskey/requests/sign_in.dart";
 import "package:kaiteki/fediverse/backends/misskey/requests/timeline.dart";
 import "package:kaiteki/fediverse/backends/misskey/responses/check_session.dart";
@@ -673,5 +675,23 @@ class MisskeyClient {
           (detail ? misskey.User.fromJson : misskey.UserLite.fromJson)
               .fromResponseList,
         );
+  }
+
+  Future<List<Mute>> getMutedAccounts(
+    String? sinceId,
+    String? untilId, {
+    int limit = 30,
+  }) async {
+    return client
+        .sendRequest(
+          HttpMethod.post,
+          "api/mute/list",
+          body: {
+            "limit": limit,
+            if (sinceId != null) "sinceId": sinceId,
+            if (untilId != null) "untilId": untilId,
+          }.jsonBody,
+        )
+        .then(Mute.fromJson.fromResponseList);
   }
 }
---
+++ src/kaiteki/lib/fediverse/backends/misskey/model/mute.dart
@@ -0,0 +1,26 @@
+import "package:fediverse_objects/misskey.dart";
+import "package:json_annotation/json_annotation.dart";
+import "package:kaiteki/utils/utils.dart";
+
+part "mute.g.dart";
+
+@JsonSerializable()
+class Mute {
+  final String id;
+  final DateTime createdAt;
+  final DateTime? expiresAt;
+  final String muteeId;
+  final User mutee;
+
+  const Mute({
+    required this.id,
+    required this.createdAt,
+    required this.muteeId,
+    required this.mutee,
+    this.expiresAt,
+  });
+
+  factory Mute.fromJson(JsonMap json) => _$MuteFromJson(json);
+
+  JsonMap toJson() => _$MuteToJson(this);
+}
