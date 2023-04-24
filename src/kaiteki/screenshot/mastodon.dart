ff1526e555cbc27b69134f073e782d6dfe558ffc
Craftplacer <craftplacer@craftplacer.moe>
2023-04-24 22:26:59 +0200

    fix: add missing changes to dummy adapters

--- src/kaiteki/screenshot/mastodon.dart
+++ src/kaiteki/screenshot/mastodon.dart
@@ -1,3 +1,4 @@
+import "package:kaiteki/fediverse/api_type.dart";
 import "package:kaiteki/fediverse/backends/mastodon/adapter.dart";
 import "package:kaiteki/fediverse/backends/mastodon/client.dart";
 import "package:kaiteki/fediverse/backends/mastodon/shared_adapter.dart";
@@ -7,10 +8,18 @@ import "package:kaiteki/fediverse/model/timeline_query.dart";
 
 class TrendingMastodonAdapter extends MastodonAdapter {
   factory TrendingMastodonAdapter(String instance) {
-    return TrendingMastodonAdapter.custom(instance, MastodonClient(instance));
+    return TrendingMastodonAdapter.custom(
+      ApiType.mastodon,
+      instance,
+      MastodonClient(instance),
+    );
   }
 
-  TrendingMastodonAdapter.custom(super.instance, super.client) : super.custom();
+  TrendingMastodonAdapter.custom(
+    super.type,
+    super.instance,
+    super.client,
+  ) : super.custom();
 
   @override
   Future<List<Post>> getTimeline(
