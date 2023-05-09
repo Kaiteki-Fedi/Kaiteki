1404d1b696f31fdeb22914ef00dd5be138aa98c9
Craftplacer <craftplacer@craftplacer.moe>
2023-05-09 05:12:38 +0200

    fix: rename experiment

--- src/kaiteki/lib/preferences/app_experiment.dart
+++ src/kaiteki/lib/preferences/app_experiment.dart
@@ -11,9 +11,9 @@ enum AppExperiment {
     "Denser reactions",
     "Reduces the spacing between reactions in order to show more of them",
   ),
-  newUserScreen(
-    "New User Screen",
-    "Use the new design of the user screen",
+  oldUserScreen(
+    "Old User Screen",
+    "Use the old design of the user screen",
   ),
   feedback(
     "App Feedback",
