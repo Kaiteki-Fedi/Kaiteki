39920a94d4dd5b8981ae392000eb1ba8d2eef5b2
Craftplacer <craftplacer@craftplacer.moe>
2023-07-15 16:50:31 +0200

    fix: use "app support" path for hive databases
    fixes #405
    
    also includes automatic migration

--- src/kaiteki/lib/main.dart
+++ src/kaiteki/lib/main.dart
@@ -1,8 +1,9 @@
 import "dart:async";
+import "dart:io";
 
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
-import "package:hive_flutter/hive_flutter.dart";
+import "package:hive/hive.dart";
 import "package:kaiteki/account_manager.dart";
 import "package:kaiteki/app.dart";
 import "package:kaiteki/di.dart";
@@ -13,6 +14,8 @@ import "package:kaiteki/theming/default/themes.dart";
 import "package:kaiteki/ui/shared/crash_screen.dart";
 import "package:kaiteki_core/utils.dart";
 import "package:logging/logging.dart";
+import "package:path/path.dart" as path;
+import "package:path_provider/path_provider.dart";
 import "package:shared_preferences/shared_preferences.dart";
 
 /// Main entrypoint.
@@ -26,6 +29,7 @@ Future<void> main() async {
 
     // initialize hive & account manager
     await initializeHive();
+    await migrateHiveBoxes();
     final accountManager = await getAccountManager();
 
     // Initialize shared preferences
@@ -56,9 +60,69 @@ void handleFatalError(TraceableError error) {
   runApp(crashScreen);
 }
 
+Future<bool> migrateHiveBoxes() async {
+  const boxNames = [
+    ("accountsecrets", "account-secrets"),
+    ("clientsecrets", "client-secrets"),
+  ];
+
+  final documents = await getApplicationDocumentsDirectory();
+  final appSupport = await getApplicationSupportDirectory();
+
+  var boxMigrated = false;
+
+  // tampering with FS is probably not possible here
+  if (kIsWeb || Platform.isAndroid || Platform.isIOS) {
+    Future<void> migrateBox<T>(Box<T> from, Box<T> to) async {
+      for (final key in from.keys) {
+        await to.put(key, from.get(key) as T);
+        await from.delete(key);
+      }
+
+      await to.close();
+
+      assert(from.isEmpty);
+      await from.close();
+      await Hive.deleteBoxFromDisk(from.name);
+    }
+
+    for (final (from, to) in boxNames) {
+      final boxExists = await Hive.boxExists(from, path: documents.path);
+      if (!boxExists) continue;
+
+      final fromBox = await Hive.openBox(from, path: documents.path);
+      final toBox = await Hive.openBox(to, path: appSupport.path);
+      await migrateBox(fromBox, toBox);
+
+      boxMigrated = true;
+    }
+
+    return boxMigrated;
+  }
+
+  for (final (sourceName, destinationName) in boxNames) {
+    for (final ext in [".hive", ".lock"]) {
+      final sourcePath = path.join(documents.path, "$sourceName$ext");
+      final destinationPath =
+          path.join(appSupport.path, "$destinationName$ext");
+
+      final file = File(sourcePath);
+
+      if (await file.exists()) {
+        await file.rename(destinationPath);
+        boxMigrated = true;
+      }
+    }
+  }
+
+  return boxMigrated;
+}
+
 Future<void> initializeHive() async {
-  await Hive.initFlutter();
+  final appSupportDir = await getApplicationSupportDirectory();
+
   Hive
+    ..init(appSupportDir.path)
     ..registerAdapter(AccountKeyAdapter())
     ..registerAdapter(ClientSecretAdapter())
     ..registerAdapter(AccountSecretAdapter());
@@ -69,7 +133,7 @@ Future<AccountManager> getAccountManager() async {
   AccountKey fromHive(dynamic k) => AccountKey.fromUri(k as String);
   String toHive(AccountKey k) => k.toUri().toString();
 
-  final accountBox = await Hive.openBox<AccountSecret>("accountSecrets");
+  final accountBox = await Hive.openBox<AccountSecret>("account-secrets");
   final accountRepository = HiveRepository<AccountSecret, AccountKey>(
     accountBox,
     fromHive,
@@ -77,7 +141,7 @@ Future<AccountManager> getAccountManager() async {
     true,
   );
 
-  final clientBox = await Hive.openBox<ClientSecret>("clientSecrets");
+  final clientBox = await Hive.openBox<ClientSecret>("client-secrets");
   final clientRepository = HiveRepository<ClientSecret, AccountKey>(
     clientBox,
     fromHive,
