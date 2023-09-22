51bee2de184e11f9cea02623e0ebe8becf720e56
Craftplacer <craftplacer@waifu.club>
2023-09-22 05:14:37 +0200

    fix: add missing bracket

--- src/kaiteki/lib/ui/instance_vetting/bottom_sheet.dart
+++ src/kaiteki/lib/ui/instance_vetting/bottom_sheet.dart
@@ -243,6 +243,7 @@ class _InstanceVettingBottomSheetState
           Tab(text: "Staff"),
           Tab(text: "Software"),
         ],
+      ),
       AutoScaleTabBarView(
         children: [
           Padding(
