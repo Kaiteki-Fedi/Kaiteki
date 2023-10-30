f78a2709a71d485db36ee23fab2fdbbec2b5d957
Craftplacer <22963120+Craftplacer@users.noreply.github.com>
2023-10-30 05:37:33 +0100

    feat: simple support for <li>

--- src/kaiteki/lib/text/parsers/html_text_parser.dart
+++ src/kaiteki/lib/text/parsers/html_text_parser.dart
@@ -3,6 +3,7 @@ import "package:html/dom.dart" as dom;
 import "package:html/parser.dart" show parseFragment;
 import "package:kaiteki/text/elements.dart";
 import "package:kaiteki/text/parsers.dart";
+import "package:kaiteki/ui/shared/common.dart";
 import "package:kaiteki/utils/extensions.dart";
 import "package:kaiteki_core/model.dart";
 import "package:logging/logging.dart";
@@ -32,6 +33,7 @@ class HtmlTextParser implements TextParser {
     "pre": _renderCodeFont,
     "span": _renderNoop,
     "strong": _renderBold,
+    "li": _renderListItem,
   };
 
   const HtmlTextParser();
@@ -130,6 +132,13 @@ class HtmlTextParser implements TextParser {
     }
 
     return [TextElement(text, children: subElements)];
+  }
+
+  static List<Element> _renderListItem(
+    dom.Element element,
+    List<Element> subElements,
+  ) {
+    return [TextElement("$kBullet ${element.text}\n")];
   }
 
   static List<Element> _renderNoop(dom.Element _, List<Element> subElements) {
