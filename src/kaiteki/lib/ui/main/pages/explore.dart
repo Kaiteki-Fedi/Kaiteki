1d5e6996f6f6fba1e32582a887751b5085a5863b
Craftplacer <22963120+Craftplacer@users.noreply.github.com>
2023-12-03 22:00:34 +0100

    feat(ui): decluttered explore page

--- src/kaiteki/lib/ui/main/pages/explore.dart
+++ src/kaiteki/lib/ui/main/pages/explore.dart
@@ -1,14 +1,17 @@
+import "dart:math";
+
 import "package:flutter/material.dart";
-import "package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart";
 import "package:go_router/go_router.dart";
 import "package:kaiteki/di.dart";
 import "package:kaiteki/ui/shared/common.dart";
 import "package:kaiteki/ui/shared/posts/post_widget.dart";
-import "package:kaiteki/ui/window_class.dart";
 import "package:kaiteki/utils/extensions.dart";
 import "package:kaiteki_core/social.dart";
 import "package:kaiteki_core/utils.dart";
+import "package:sliver_tools/sliver_tools.dart";
 import "package:url_launcher/url_launcher.dart";
+
+const _kPostLimit = 3;
 
 final trendingLinksProvider = FutureProvider<List<Embed>?>(
   (ref) async {
@@ -49,156 +52,175 @@ class ExplorePage extends ConsumerStatefulWidget {
 }
 
 class _ExplorePageState extends ConsumerState<ExplorePage> {
+  bool _expandPosts = false;
+
   @override
   Widget build(BuildContext context) {
-    final cards = buildCards(context);
-    final windowClass = WindowClass.fromContext(context);
-
-    if (windowClass <= WindowClass.medium) {
-      return ListView(
-        padding: const EdgeInsets.all(16.0),
-        children: cards.joinWithValue(const SizedBox(height: 16)),
-      );
-    }
-
-    return MasonryGridView.count(
-      padding: const EdgeInsets.all(16.0),
-      crossAxisCount: 2,
-      mainAxisSpacing: 16,
-      crossAxisSpacing: 8,
-      itemBuilder: (context, index) => cards[index],
-      itemCount: cards.length,
-    );
-  }
-
-  List<Widget> buildCards(BuildContext context) {
     final explore = ref.watch(adapterProvider).safeCast<ExploreSupport>();
-    return <Widget>[
-      buildTrendingPosts(context),
-      // buildTrendingHashtags(context),
-      if (explore?.capabilities.supportsTrendingLinks ?? false)
-        buildNews(context),
-    ];
-  }
 
-  Widget buildNews(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text(
-          "News",
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
-        const SizedBox(height: 16),
-        ref.watch(trendingLinksProvider).map(
-              data: (data) {
-                final cards = data.value
-                    ?.map<Widget>((e) => NewsCard(embed: e))
-                    .toList()
-                    .joinWithValue(const SizedBox(height: 8));
-                return Column(
-                  children: cards ?? [],
-                );
-              },
-              error: (_) => const SizedBox(),
-              loading: (_) => Column(
-                children: <Widget>[
-                  for (int i = 0; i < 3; i++) const NewsCard(embed: null),
-                ].joinWithValue(const SizedBox(height: 8)),
-              ),
+    return CustomScrollView(
+      primary: true,
+      slivers: [
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+          sliver: SliverCrossAxisConstrained(
+            maxCrossAxisExtent: 600,
+            child: SliverMainAxisGroup(
+              slivers: [
+                ...buildTrendingPosts(context),
+                if (explore?.capabilities.supportsTrendingLinks ?? false)
+                  ...buildNews(context),
+              ],
             ),
+          ),
+        ),
       ],
     );
   }
 
-  Widget buildTrendingPosts(BuildContext context) {
-    final trendingPosts = ref.watch(trendingPostsProvider);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text(
+  Iterable<Widget> buildNews(BuildContext context) sync* {
+    yield const SliverToBoxAdapter(child: SizedBox(height: 16));
+
+    yield SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          "News",
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      ),
+    );
+
+    yield const SliverToBoxAdapter(child: SizedBox(height: 16));
+
+    final trendingLinks = ref.watch(trendingLinksProvider);
+    yield trendingLinks.map(
+      data: (data) {
+        return SliverList.separated(
+          itemBuilder: (_, i) => NewsCard(embed: data.value![i]),
+          separatorBuilder: (_, __) => const SizedBox(height: 8),
+          itemCount: data.value?.length ?? 0,
+        );
+      },
+      error: (_) => const SliverToBoxAdapter(child: SizedBox()),
+      loading: (_) => SliverToBoxAdapter(
+        child: Column(
+          children: <Widget>[
+            for (int i = 0; i < 3; i++) const NewsCard(embed: null),
+          ].joinWithValue(const SizedBox(height: 8)),
+        ),
+      ),
+    );
+  }
+
+  Iterable<Widget> buildTrendingPosts(BuildContext context) sync* {
+    yield SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
           "Trending right now",
           style: Theme.of(context).textTheme.headlineSmall,
         ),
-        const SizedBox(height: 16),
-        trendingPosts.map(
-          data: (data) => Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              ref.watch(trendingHashtagsProvider).map(
-                    data: (data) => Wrap(
-                      spacing: 8,
-                      runSpacing: 8,
-                      children: <Widget>[
-                        for (final hashtag in data.value!)
-                          ActionChip(
-                            // ignore: l10n
-                            label: Text("#$hashtag"),
-                            onPressed: () {
-                              context.pushNamed(
-                                "hashtag",
-                                pathParameters: {
-                                  ...ref.accountRouterParams,
-                                  "hashtag": hashtag,
-                                },
-                              );
-                            },
-                          ),
-                      ],
-                    ),
-                    error: (_) => const SizedBox(),
-                    loading: (_) => const Row(),
-                  ),
-              const SizedBox(height: 8),
-              for (final post in data.value!.take(3))
-                Card(
-                  child: PostWidget(
-                    post,
-                    layout: PostWidgetLayout.wide,
-                  ),
-                ),
-            ].joinWithValue(const SizedBox(height: 8)),
-          ),
-          error: (_) => const SizedBox(),
-          loading: (_) => const Padding(
+      ),
+    );
+    yield const SliverToBoxAdapter(child: SizedBox(height: 16));
+
+    final trendingHashtags = ref.watch(trendingHashtagsProvider);
+    yield trendingHashtags.map(
+      data: (data) => SliverToBoxAdapter(
+        child: Wrap(
+          spacing: 8.0,
+          runSpacing: 8.0,
+          children: [
+            for (int i = 0; i < data.value!.length; i++)
+              _HashtagChip(data.value![i]),
+          ],
+        ),
+      ),
+      error: (_) => const SliverToBoxAdapter(child: SizedBox()),
+      loading: (_) => SliverToBoxAdapter(
+        child: Wrap(
+          spacing: 8.0,
+          runSpacing: 8.0,
+          children: [
+            for (int i = 0; i < 10; i++)
+              // ignore: l10n
+              const ActionChip(label: Text("...")),
+          ],
+        ),
+      ),
+    );
+
+    yield const SliverToBoxAdapter(child: SizedBox(height: 8));
+
+    final trendingPosts = ref.watch(trendingPostsProvider);
+    if (trendingPosts.valueOrNull != null) {
+      yield trendingPosts.map(
+        data: (data) {
+          final posts = data.requireValue!;
+          return SliverList.separated(
+            itemCount:
+                _expandPosts ? posts.length : min(posts.length, _kPostLimit),
+            itemBuilder: (context, index) {
+              final post = posts[index];
+              return PostWidget(
+                post,
+                onOpen: () => context.showPost(post, ref),
+              );
+            },
+            separatorBuilder: (_, __) => const SizedBox(height: 8),
+          );
+        },
+        error: (_) => const SliverToBoxAdapter(child: SizedBox()),
+        loading: (_) => const SliverToBoxAdapter(
+          child: Padding(
             padding: EdgeInsets.symmetric(vertical: 8.0),
             child: LinearProgressIndicator(),
           ),
         ),
-      ],
+      );
+
+      final postCount = trendingPosts.value!.length;
+      if (postCount > _kPostLimit) {
+        yield SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Center(
+              child: TextButton(
+                onPressed: () => setState(() => _expandPosts = !_expandPosts),
+                child: _expandPosts
+                    ? Text("Show fewer posts")
+                    : Text("Show ${postCount - _kPostLimit} more"),
+              ),
+            ),
+          ),
+        );
+      }
+    }
+  }
+}
+
+class _HashtagChip extends ConsumerWidget {
+  const _HashtagChip(this.hashtag, {super.key});
+
+  final String hashtag;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ActionChip(
+      // ignore: l10n
+      label: Text("#$hashtag"),
+      onPressed: () {
+        context.pushNamed(
+          "hashtag",
+          pathParameters: {
+            ...ref.accountRouterParams,
+            "hashtag": hashtag,
+          },
+        );
+      },
     );
   }
-
-// Widget buildTrendingHashtags(BuildContext context) {
-//   return Column(
-//     crossAxisAlignment: CrossAxisAlignment.stretch,
-//     children: [
-//       Text(
-//         "Trends",
-//         style: Theme.of(context).textTheme.headlineSmall,
-//       ),
-//       const SizedBox(height: 16),
-//       FutureBuilder(
-//         future: ,
-//         builder: (context, snapshot) {
-//           if (!snapshot.hasData) return const SizedBox();
-//
-//           final hashtags = snapshot.data!.take(5).toList();
-//           return Card(
-//             child: Column(
-//               children: <Widget>[
-//                 for (var i = 0; i < hashtags.length; i++)
-//                   ListTile(
-//                     title: Text("#${hashtags[i]}"),
-//                   ),
-//               ].joinWithValue(const SizedBox(height: 8)),
-//             ),
-//           );
-//         },
-//       ),
-//     ],
-//   );
-// }
 }
 
 class NewsCard extends StatelessWidget {
