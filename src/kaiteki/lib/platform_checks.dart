b6619217077dc4e9249be217869f4f332d5c7e58
Craftplacer <22963120+Craftplacer@users.noreply.github.com>
2023-12-28 15:43:11 +0100

    refactor: remove unused platform consts/checks

--- src/kaiteki/lib/platform_checks.dart
+++ src/kaiteki/lib/platform_checks.dart
@@ -1,14 +1,5 @@
-import "dart:io";
-
-import "package:flutter/foundation.dart";
-
 bool get supportsVideoPlayer => kIsWeb || _isMobileOS;
 
 const kBuildFlavor = bool.hasEnvironment("BUILD_FLAVOR")
     ? String.fromEnvironment("BUILD_FLAVOR")
     : null;
-
-// HACK(Craftplacer): I'd prefer an actual system call to check whether a hard keyboard is attached
-bool get hasPhysicalKeyboard => kIsWeb || !_isMobileOS;
-
-bool get _isMobileOS => Platform.isAndroid || Platform.isIOS;
