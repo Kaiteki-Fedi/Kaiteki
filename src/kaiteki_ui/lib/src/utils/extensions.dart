13b44c6dda3490e5cbd794790484ca87f22029a4
Craftplacer <22963120+Craftplacer@users.noreply.github.com>
2023-12-31 02:16:19 +0100

    refactor: remove EdgeInsetsDirectional.copyWith extension

--- src/kaiteki_ui/lib/src/utils/extensions.dart
+++ src/kaiteki_ui/lib/src/utils/extensions.dart
@@ -14,20 +14,3 @@ extension DeflateExtensions on Rect {
 extension ColorTextStyleExtension on Color {
   TextStyle get textStyle => TextStyle(color: this);
 }
-
-extension KaitekiEdgeInsetsDirectionalExtension on EdgeInsetsDirectional {
-  // HACK(Craftplacer): https://github.com/flutter/flutter/issues/137475
-  EdgeInsetsDirectional copyWith({
-    double? start,
-    double? top,
-    double? end,
-    double? bottom,
-  }) {
-    return EdgeInsetsDirectional.only(
-      start: start ?? this.start,
-      top: top ?? this.top,
-      end: end ?? this.end,
-      bottom: bottom ?? this.bottom,
-    );
-  }
-}
