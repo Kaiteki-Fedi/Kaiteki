7a25b68b2fa624776451e8b89abff2c4f92c9de2
Craftplacer <22963120+Craftplacer@users.noreply.github.com>
2024-02-02 08:39:18 +0100

    fix: fallback to utf-8 when no charset is provided
    addresses https://github.com/superseriousbusiness/gotosocial/issues/2598

--- src/kaiteki_core/lib/src/http/extensions.dart
+++ src/kaiteki_core/lib/src/http/extensions.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:http_parser/http_parser.dart';
 import 'package:kaiteki_core/utils.dart';
 
 typedef DeserializeFromJson<T> = T Function(JsonMap json);
@@ -9,15 +10,37 @@ typedef GenericDeserializeFromJson<T, K> = T Function(
   K Function(Object?) jsonFromT,
 );
 
+Encoding _getEncodingFromHeaders(Map<String, String> headers) {
+  final contentType = headers['content-type'];
+
+  if (contentType == null) return utf8;
+
+  try {
+    // This can throw. Thanks Google 👍
+    final parsed = MediaType.parse(contentType);
+    final charset = parsed.parameters['charset'];
+    final encoding = Encoding.getByName(charset);
+    if (encoding != null) return encoding;
+  } catch (e) {
+    // TODO(Craftplacer): log error
+  }
+
+  return utf8;
+}
+
 extension KaitekiResponseExtensions on Response {
   bool get isSuccessful => !(400 <= statusCode && statusCode < 600);
 
   T fromJson<T>(DeserializeFromJson<T> deserialize) {
+    final encoding = _getEncodingFromHeaders(headers);
+    final body = encoding.decode(bodyBytes);
     final json = jsonDecode(body) as JsonMap;
     return deserialize(json);
   }
 
   List<T> fromJsonList<T>(DeserializeFromJson<T> deserialize) {
+    final encoding = _getEncodingFromHeaders(headers);
+    final body = encoding.decode(bodyBytes);
     final json = (jsonDecode(body) as List<dynamic>).cast<JsonMap>();
     return json.map(deserialize).toList();
   }
@@ -26,6 +49,7 @@ extension KaitekiResponseExtensions on Response {
 extension KaitekiJsonDeserializationResopnseExtensions<T>
     on DeserializeFromJson<T> {
   T fromResponse(Response response) => response.fromJson(this);
+
   List<T> fromResponseList(Response response) => response.fromJsonList(this);
 }
 
