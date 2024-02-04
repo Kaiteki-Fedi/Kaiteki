a161bac5da7bb14744ae604f23d8b5f8049a684c
Craftplacer <22963120+Craftplacer@users.noreply.github.com>
2024-02-04 13:37:54 +0100

    feat: remove tooltip from extended fab

--- src/kaiteki/lib/ui/main/tabs/home.dart
+++ src/kaiteki/lib/ui/main/tabs/home.dart
@@ -11,7 +11,6 @@ class _HomeMainScreenTab extends MainScreenTab {
     final l10n = context.l10n;
     return FloatingActionButton.extended(
       icon: const Icon(Icons.edit_rounded),
-      tooltip: l10n.composeDialogTitle,
       label: Text(l10n.composeButtonLabel),
       onPressed: () => context.pushNamed(
         "compose",
