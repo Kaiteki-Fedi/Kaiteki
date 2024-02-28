534ee375098a80541512d5bc2895a8b8af95a06d
Craftplacer <22963120+Craftplacer@users.noreply.github.com>
2024-02-28 18:31:59 +0100

    feat: ui spacing exts

--- packages/kaiteki_ui/lib/src/utils/extensions.dart
+++ packages/kaiteki_ui/lib/src/utils/extensions.dart
@@ -14,3 +14,25 @@ extension DeflateExtensions on Rect {
 extension ColorTextStyleExtension on Color {
   TextStyle get textStyle => TextStyle(color: this);
 }
+
+extension WidgetListExtension on List<Widget> {
+  List<Widget> spacedHorizontally(double gap) {
+    return [
+      for (var i = 0; i< length; i++) ...[
+        if (i != 0) SizedBox(width: gap),
+        this[i],
+      ],
+
+        ];
+  }
+
+  List<Widget> spacedVertically(double gap) {
+    return [
+      for (var i = 0; i< length; i++) ...[
+        if (i != 0) SizedBox(height: gap),
+        this[i],
+      ],
+
+    ];
+  }
+}
